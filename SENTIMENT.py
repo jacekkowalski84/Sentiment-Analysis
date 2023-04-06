527b871b615cf3b665d750a5273822f60bc9629e
Jacek Kowalski <JACEK.KOWALSKI1984@GMAIL.COM>
2023-04-07 00:37:35 +0200

    Refactor: Enhance program modularity by implementing functions
    Refactored program by dividing it into smaller, reusable functions. Implemented the use of the "__name__" variable to ensure proper execution when running the program.

--- SENTIMENT.py
+++ SENTIMENT.py
@@ -1,69 +1,68 @@
 import glob
 
-INTERPUNCTION = ".,!?%()[]{}\"-_"
+
 FILES_POSITIVE= glob.glob (r"data\pos\*.txt")
 FILES_NEGATIVE= glob.glob (r"data\neg\*.txt")
 
 
-reviews_positive = {}
-reviews_negative = {}
+def text_conversion (text):
+    INTERPUNCTION = ".,!?%()[]{}\"-_"
+    for char in INTERPUNCTION:
+        text = text.replace (char, " ")
+    text = text.lower().replace("<br />", " ").split()
 
-#CREATING LIST OF POSITIVE REVIEWS
 
-for file_positive in FILES_POSITIVE:
-    with open (file_positive, encoding='utf-8-sig') as stream: #dopisanie argumentu encoding znalazłem w podowiedziach na slacku
-        record = stream.read ()
-        for char in INTERPUNCTION:
-            record = record.replace (char, " ")
-        record = record.replace ("<br />", " ")
-        record = set(record.lower().split())
-        for word in record:
-            if word not in reviews_positive:
-                reviews_positive[word] = 0
-            reviews_positive[word] += 1
+def reviews_to_list (files_path):
+    reviews = {}
+    for file in files_path:
+        with open (file, encoding='utf-8-sig') as stream: 
+            record = stream.read ()
+            text_conversion(record)
+            for word in record:
+                reviews [word] = reviews.get(word,0)
+                reviews [word] += 1
+    return (reviews)
             
 
-#CREATING LIST OF NEGATIVE REVIEWS
+def new_review(): 
+    new_review = input ("Add new review: ")
+    text_conversion(new_review)
+    return (new_review)
 
-for file_negative in FILES_NEGATIVE:
-    with open (file_negative, encoding='utf-8-sig') as stream: #dopisanie argumentu encoding znalazłem w podowiedziach na slacku
-        record = stream.read ()
-        for char in INTERPUNCTION:
-            record = record.replace (char, " ")
-        record = record.replace ("<br />", " ")
-        record = set(record.lower().split())
-        for word in record:
-            if word not in reviews_negative:
-                reviews_negative[word] = 0
-            reviews_negative [word] += 1
 
-#ASKING USER FOR A NEW REVIEW AND CHANGING IT TO LIST
- 
-new_review = input ("Add new review: ")
-for char in INTERPUNCTION:
-    new_review = new_review.replace (char, " ")
-new_review = new_review.lower().split()
-
-#CALCULATING SENTIMENT FOR EACH INDIVIDUAL WORD IN NEW REVIEW
-
-sentiment_count = []
-
-for new_word in new_review:
-    positive_count = reviews_positive.get(new_word, 0)
-    negative_count = reviews_negative.get(new_word, 0)
-    if (positive_count + negative_count) == 0:
-        print ("Sentiment of: \"", new_word, "\"= 0.00")
-    else:
-        word_sentiment = (positive_count - negative_count)/(positive_count + negative_count)
-        sentiment_count.append (word_sentiment)
-        print ("Sentiment of: \"", new_word, "\"= ", word_sentiment)
+def word_sentiment (pos_reviews, neg_reviews, new_review):
+    sentiment_count = []
+    for new_word in new_review:
+        positive_count = pos_reviews.get(new_word, 0)
+        negative_count = neg_reviews.get(new_word, 0)
+        if (positive_count + negative_count) == 0:
+            print ("Sentiment of: \"", new_word, "\"= 0.00")
+        else:
+            word_sentiment = (positive_count - negative_count)/(positive_count + negative_count)
+            print ("Sentiment of: \"", new_word, "\"= ", word_sentiment)
+            sentiment_count.append (word_sentiment)
+    return (sentiment_count)
     
-# CALCULATING SENTIMENT FOR THE WHOLE NEW REVIEW
+def review_sentiment (word_sentiment):
+    sentiment_average = sum(word_sentiment)
+    sentiment_average = len.word_sentiment()
+    if sentiment_average > 0:
+        label = "positive"
+    else:
+        label = "negative"
 
-sentiment_average = sum(sentiment_count)
-if sentiment_average > 0:
-    label = "positive"
-else:
-    label = "negative"
+    print ("This sentence is ", label , sentiment_average)
 
-print ("This sentence is ", label , sentiment_average)
+
+def main():
+    pos = reviews_to_list (FILES_POSITIVE)
+    neg = reviews_to_list (FILES_NEGATIVE)
+    new = new_review()
+    ws = word_sentiment(pos, neg, new)
+    review_sentiment(ws)
+
+
+if __name__ == "__main__":
+    main()
+    print ("This is the end.")
+
